2617b691f181e38b1c06bdbe04e6899e5dcf93ac
Niklas Schildhauer <ns162@hdm-stuttgart.de>
2022-04-13 09:16:37 +0200

    bardocde scanner

---
+++ iOS/Trittstufe/Trittstufe/ViewController/QRCodeScannerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QRCodeScannerViewController.swift
+//  Trittstufe
+//
+//  Created by Niklas Schildhauer on 13.04.22.
+//
+
+import UIKit
+
+class QRCodeScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
